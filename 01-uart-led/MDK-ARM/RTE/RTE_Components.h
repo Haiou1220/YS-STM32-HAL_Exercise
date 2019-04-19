5d287e77be9ee807b8ef3290ab24ec5c36723159
haiou <guohaihang@outlook.com>
2019-04-20 01:11:14 +0800

    第一个project

---
+++ 01-uart-led/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'uart-led' 
+ * Target:  'uart-led' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
